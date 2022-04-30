c81ab4e5b43f670b825f3864752ee6310eb69d1c
Ian Johnston <ianallanjohnston@gmail.com>
2022-04-29 19:00:46 -0700

    completed mvp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import Navbar from "./components/Navbar";
-
+import { useAuthContext } from "./hooks/useAuthContext";
 function App() {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path='/'>
+              {!user && <Redirect to='/login' />}
+              {user && <Home />}
+            </Route>
+
+            <Route exact path='/signup'>
+              {!user && <Signup />}
+              {user && <Redirect to='/' />}
+            </Route>
+            <Route exact path='/login'>
+              {!user && <Login />}
+              {user && <Redirect to='/' />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
